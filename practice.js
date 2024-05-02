352d5d641219fedc7adbdf9f885ae3c9b9fd6910
raeezrazz <mhdrahees67@gmail.com>
2024-05-02 14:25:36 +0530

    two sum

---
+++ practice.js
@@ -0,0 +1,22 @@
+
+//   TWO SUM
+
+// const arr = [6,7,8,4,5,1,0]
+// const target = 10
+// function twoSum(arr,target){
+
+//     const s = new Set
+
+// for(let i = 0 ; i < arr.length ; i ++){
+//     let k = arr[i]
+//     let remaining = target - k
+//     if(s.has(remaining)){
+//         return b =[k,remaining]
+//     }else{
+//         s.add(k)
+//     }
+// }
+// }
+
+// let result = twoSum(arr,target)
+// console.log(result)
