15738942dd939876482f256eabed30086dd9339b
raeezrazz <mhdrahees67@gmail.com>
2024-05-17 12:07:32 +0530

    new practical

--- sorting/bubble.js
+++ sorting/bubble.js
@@ -1,16 +1,36 @@
-let arr = [3,4,6,1,33,9,2,8,214,56,875,133]
+// let arr = [3,4,6,1,33,9,2,8,214,56,875,133]
 
-function bubble(arr){
-    for(let i = 0 ; i < arr.length  ; i++){
-        for(let j = 0 ; j < arr.length -1-i ; j ++){
+// function bubble(arr){
+//     for(let i = 0 ; i < arr.length  ; i++){
+//         for(let j = 0 ; j < arr.length -1-i ; j ++){
+//             if(arr[j]>arr[j+1]){
+//                 tem = arr[j]
+//                 arr[j]= arr[j+1]
+//                 arr[j+1] = tem
+//             }
+//         }
+//     }
+//     return arr
+// }
+
+// console.log(bubble(arr))
+
+
+
+// practicing
+
+let arr =[4,52,7,7,225,2656,265425,524,4,454,543,3]
+
+function bubbleSort(arr){
+    for(let i =0; i < arr.length -1 ; i ++){
+        for(let j = 0 ; j < arr.length -i-1; j++){
             if(arr[j]>arr[j+1]){
-                tem = arr[j]
-                arr[j]= arr[j+1]
-                arr[j+1] = tem
+                let temp = arr[j]
+                arr[j]=arr[j+1]
+                arr[j+1]=temp
             }
         }
     }
     return arr
 }
-
-console.log(bubble(arr))
+console.log(bubbleSort(arr))
--- sorting/insertion.js
+++ sorting/insertion.js
@@ -1,17 +1,37 @@
-let arr = [3,4,6,1,33,9,2,8,214,56,875,133]
+// let arr = [3,4,6,1,33,9,2,8,214,56,875,133]
 
+
+// function insertion(arr){
+//     for(let i =0 ; i < arr.length ; i ++){
+//         let curr = arr[i]
+//         let j = i -1
+//         while( j >= 0 &&arr[j]> curr){ 
+//             arr[j+1] = arr[j]
+//             j--
+//         }
+//         arr[j+1] =curr
+       
+//     }
+//     return arr
+// }
+// console.log(insertion(arr));
+
+
+
+
+let arr = [13,3242,45,56476,87687,84,635,4,667,67346,4,252,435]
 
 function insertion(arr){
-    for(let i =0 ; i < arr.length ; i ++){
-        let curr = arr[i]
-        let j = i -1
-        while( j >= 0 &&arr[j]> curr){
-            arr[j+1] = arr[j]
+    for(let i =0 ; i < arr.length ; i++){
+        let curr =arr[i]
+        let j = i-1
+        while(j>=0 && arr[j]>curr){
+            arr[j+1]=arr[j]
             j--
         }
-        arr[j+1] =curr
-       
+        arr[j+1]=curr
     }
     return arr
 }
-console.log(insertion(arr));
+
+console.log(insertion(arr))
